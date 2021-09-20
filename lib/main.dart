a54eea78fd7774350b879d4cc0e99e39ecc4eb0f
xandeft <alexandre-ft2@hotmail.com>
2021-09-20 01:06:21 -0300

    Add funcionalities Task 3

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       title: 'Welcome to Flutter',
       theme: ThemeData(primaryColor: Colors.white),
       home: RandomWords(),
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWords extends StatefulWidget {
-  const RandomWords({ Key? key }) : super(key: key);
+  const RandomWords({Key? key}) : super(key: key);
 
   @override
   _RandomWordsState createState() => _RandomWordsState();
@@ -39,17 +39,15 @@ class _RandomWordsState extends State<RandomWords> {
         }
         final item = _suggestions[index].asPascalCase;
         return Dismissible(
-          key: Key(item),
-          onDismissed: (direction) {
-            setState(() {
-              _saved.remove(_suggestions[index]);
-              _suggestions.removeAt(index);
-            });
-          },
-          background: Container(color: Colors.red),
-          child: _buildRow(_suggestions[index])
-        );
-        //_buildRow(_suggestions[index]);
+            key: Key(item),
+            onDismissed: (direction) {
+              setState(() {
+                _saved.remove(_suggestions[index]);
+                _suggestions.removeAt(index);
+              });
+            },
+            background: Container(color: Colors.red),
+            child: _buildRow(_suggestions[index]));
       },
     );
   }
@@ -57,55 +55,157 @@ class _RandomWordsState extends State<RandomWords> {
   Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          alreadySaved ? _saved.remove(pair) : _saved.add(pair);
+        title: InkWell(
+          child: Text(pair.asPascalCase, style: _biggerFont),
+          onTap: () {
+            alreadySaved
+                ? ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: const Text('Only edit non-favorite item'),
+                    duration: Duration(seconds: 2),
+                  ))
+                : openAlertBox(pair);
+          },
+        ),
+        trailing: InkWell(
+          child: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+          ),
+          onTap: () {
+            setState(() {
+              alreadySaved ? _saved.remove(pair) : _saved.add(pair);
+            });
+          },
+        ));
+  }
+
+  openAlertBox(WordPair pair) {
+    final myController = TextEditingController();
+    final myController2 = TextEditingController();
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0))),
+              contentPadding: const EdgeInsets.only(top: 10.0),
+              content: SizedBox(
+                width: 300.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: const <Widget>[Text("Alter name item")],
+                    ),
+                    const SizedBox(
+                      height: 5.0,
+                    ),
+                    const Divider(
+                      color: Colors.grey,
+                      height: 4.0,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Enter new first string",
+                          border: InputBorder.none,
+                        ),
+                        controller: myController,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Enter new second string",
+                          border: InputBorder.none,
+                        ),
+                        controller: myController2,
+                      ),
+                    ),
+                    InkWell(
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+                        decoration: const BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(32.0),
+                              bottomRight: Radius.circular(32.0)),
+                        ),
+                        child: const Text(
+                          "OK",
+                          style: TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          if (myController.text == "" &&
+                              myController2.text == "") {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: const Text('Invalid inputs'),
+                              duration: Duration(seconds: 2),
+                            ));
+                          } else if (myController.text == null ||
+                              myController.text == "") {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: const Text('Invalid first input'),
+                              duration: Duration(seconds: 2),
+                            ));
+                          } else if (myController2.text == null ||
+                              myController2.text == "") {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: const Text('Invalid second input'),
+                              duration: Duration(seconds: 2),
+                            ));
+                          } else {
+                            _suggestions[_suggestions.indexOf(pair)] =
+                                WordPair(myController.text, myController2.text);
+                            Navigator.pop(context);
+                          }
+                        });
+                      },
+                    )
+                  ],
+                ),
+              ));
         });
-      },
-    );
   }
 
   void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final tiles = _saved.map(
+        (WordPair pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
             ),
-            body: ListView(children: divided),
           );
-        }
-      )
-    );
+        },
+      );
+      final divided = ListTile.divideTiles(
+        context: context,
+        tiles: tiles,
+      ).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved Suggestions'),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
